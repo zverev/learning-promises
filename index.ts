f8dde4251bad5a60704fa76f66cdfdafd062196f
zverev <alexander.zverev@gmail.com>
2021-01-10 15:16:10 +0300

    error handling

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 interface IPromise {
-    then: (onSuccess: () => void, onFailure?: () => void) => IPromise
+    then: (onSuccess: () => void, onFailure?: () => void) => IPromise;
+    catch: (onFailure: () => void) => IPromise;
 }
 
 const createPromiseThatFulfillsInATimeout = (timeoutSeconds: number) => (
@@ -14,18 +15,13 @@ const myPromise = createPromiseThatFulfillsInATimeout(1);
 
 const myPromise2 = myPromise.then((timeoutSeconds: number) => {
     console.log(`previous promise was fulfilled in ${timeoutSeconds} seconds`)
+    throw new Error("404")
     return createPromiseThatFulfillsInATimeout(2)
 })
 
-setTimeout(() => {
-    myPromise.then((timeoutSeconds: number) => {
-        console.log(`previous promise was fulfilled in ${timeoutSeconds} seconds`)
-    })
-    myPromise.then((timeoutSeconds: number) => {
-        console.log(`previous promise was fulfilled in ${timeoutSeconds} seconds`)
-    })
-}, 10000);
-
 const myPromise3 = myPromise2.then(() => {
     console.log("should be called right after that")
+}).catch((err) => {
+    console.error("An error occured, here we'll handle it")
+    console.error(err.message)
 })
